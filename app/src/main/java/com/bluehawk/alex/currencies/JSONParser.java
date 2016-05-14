517d19543b6954cc8a1133fbafd852fb00f6350c
Alex <xqhu2008@hotmail.com>
2016-05-14 21:05:55 +0800

    Fires-up MainActivity with Intent and passes ArrayList into Bundle.

---
+++ app/src/main/java/com/bluehawk/alex/currencies/JSONParser.java
@@ -0,0 +1,53 @@
+package com.bluehawk.alex.currencies;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+/**
+ * Created by Alex on 2016/5/14.
+ */
+public class JSONParser {
+
+    static InputStream sInputStream = null;
+    static JSONObject sReturnJsonObject = null;
+    static String sRawJsonString = "";
+
+    public JSONParser() {}
+
+    public JSONObject getJSONFromUrl(String url) {
+        // attempt to get response from server
+        try {
+            URL urlconn = new URL(url);
+            URLConnection uconn = urlconn.openConnection();
+            sInputStream = uconn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(sInputStream, "iso-8859-1"), 8);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+
+            sInputStream.close();
+            sRawJsonString = stringBuilder.toString();
+        } catch (Exception e) {
+            Log.e("Error " + e.toString(), this.getClass().getSimpleName());
+        }
+
+        try {
+            sReturnJsonObject = new JSONObject(sRawJsonString);
+        } catch (JSONException e) {
+            Log.e("Parser", "Error " + e.toString());
+        }
+
+        return sReturnJsonObject;
+    }
+}
